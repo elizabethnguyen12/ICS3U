89a64b047040bdf37650ee24963a5e4ec642b720
323827535 <323827535@C-3ZYBYR1.tdsb.on.ca>
2017-02-21 14:45:09 -0500

    pushed first program

---
+++ Hello/src/nguyen/HelloWorld.java
@@ -0,0 +1,13 @@
+package nguyen;
+/**
+ * HelloWorld.java
+ * intro
+ * February 21st, 2017
+ * @author Elizabeth Nguyen
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
