ad0d60488e8dc9bf3c0aeca9e34c0f0b04b7daa5
323827535 <323827535@C-3ZYBYR1.tdsb.on.ca>
2017-06-15 14:31:50 -0400

    committing updated drive forward program for culminating

--- NXT/src/com/mydomain/PickUpBallAndDrive.java
+++ NXT/src/com/mydomain/PickUpBallAndDrive.java
@@ -8,14 +8,17 @@ public class PickUpBallAndDrive implements Behavior{
 	private boolean suppressed = false;
 	//private boolean hasBall = false;
 	
+	@Override
 	public void suppress(){
 		suppressed = true;
 	}
 	
+	@Override
 	public boolean takeControl(){
 		return true;
 	}
 	
+	@Override
 	public void action(){
 		//if(!hasBall){//if there is no ball
 		//	Motor.A.rotate(20);//pick up ball
@@ -24,6 +27,12 @@ public class PickUpBallAndDrive implements Behavior{
 		//drive forward
 		Motor.C.forward();
 		Motor.B.forward();
+		
+		while(!suppressed){
+			Thread.yield();
+		}
+		Motor.B.stop();
+		Motor.C.stop();
 	}
 
 }
